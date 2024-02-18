c56155cf2434db71f068fad8e8d97e625f8ac2f4
Braeden Smith <braedensmith629@gmail.com>
2024-02-18 16:25:24 -0600

    Set up basic camera UI

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,8 +1,8 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, StyleSheet, AppState, AppStateStatus} from 'react-native'
 import { useIsFocused } from '@react-navigation/native';
-import {AppState, AppStateStatus} from 'react-native'
-import React, { useEffect, useState } from 'react';
-import {Text, StyleSheet } from 'react-native';
 import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
+import { Button } from 'react-native-paper';
 
 const CameraScreen = () => {
   // Handle app state
@@ -31,18 +31,53 @@ const CameraScreen = () => {
     }
   }, [hasPermission]);
 
+  // Handle flash
+  const [flash, setFlash] = useState<'on' | 'off' | 'auto'>('off')
+  function cycleFlash() {
+    if (flash === 'off') {
+      setFlash('auto')
+    } else if (flash === 'auto') {
+      setFlash('on')
+    } else {
+      setFlash('off')
+    }
+  }
+
+  // Handle camera
+  const [cameraDirection, setCameraDirection] = useState<'front' | 'back'>('back')
+  const device = useCameraDevice(cameraDirection);
+  const camera = useRef<Camera>(null)
+  async function takePhoto() {
+    const photo = await camera.current?.takePhoto({
+      flash: device?.hasFlash ? flash : 'off',
+    })
+    console.log(photo)
+  }
+
   if (hasPermission) {
-    const device = useCameraDevice('front');
     if (!device) {
       return <Text>Loading...</Text>;
     }
-    console.log(device.position);
     return (
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={isActive}
-      />
+      <View style={StyleSheet.absoluteFill}>
+        <Camera
+          ref={camera}
+          photo={true}
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={isActive}
+        />
+        <Button onPress={takePhoto} style={{position: 'absolute', bottom: 20, alignSelf: 'center', backgroundColor: 'black'}}>
+          <Text style={{color: 'white'}}>Take Photo</Text>
+        </Button>
+        <Button onPress={cycleFlash} style={{position: 'absolute', top: 20, left: 20, alignSelf: 'flex-start', backgroundColor: 'black'}}>
+          <Text style={{color: 'white'}}>Flash ( {flash} )</Text>
+        </Button>
+        <Button onPress={() => setCameraDirection(cameraDirection === 'back' ? 'front' : 'back')} style={{position: 'absolute', top: 20, right: 20, alignSelf: 'flex-end', backgroundColor: 'black'}}>
+          <Text style={{color: 'white'}}>Flip</Text>
+        </Button>
+        <Text style={{position: 'absolute', top: 20, alignSelf: 'center', backgroundColor: 'black', color: 'white', fontSize: 20}}>SNIPEme</Text>
+      </View>
     );
   } else {
     return <Text>No access to camera. Go to your settings to provide access.</Text>;
