df9b2f5cc941fd7b7ef54b269ae13809daefa67b
Braeden Smith <braedensmith629@gmail.com>
2024-02-17 23:50:18 -0600

    Set up basic camera view

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import {View, Text, Alert, Button, StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import {Text, StyleSheet, Platform, PermissionsAndroid } from 'react-native';
+import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
+import { Camera } from 'react-native-vision-camera'
 
 const CameraScreen = () => {
-  return (
-    <View style={styles.view}>
-      <Text>Camera!</Text>
-    </View>
-  );
-};
+  const [hasPermission, setHasPermission] = React.useState<boolean | null>(null);
 
-const styles = StyleSheet.create({
-  view: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'ios') {
+        const cameraPermission = await check(PERMISSIONS.IOS.CAMERA);
+        if (cameraPermission === RESULTS.DENIED) {
+          const result = await request(PERMISSIONS.IOS.CAMERA);
+          setHasPermission(result === RESULTS.GRANTED);
+        } else {
+          setHasPermission(cameraPermission === RESULTS.GRANTED);
+        }
+      } else {
+        const result = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: "Camera Permission",
+            message: "Your app needs access to your camera",
+            buttonNeutral: "Ask Me Later",
+            buttonNegative: "Cancel",
+            buttonPositive: "OK"
+          }
+        );
+        setHasPermission(result === PermissionsAndroid.RESULTS.GRANTED);
+      }
+    })();
+  }, []);
+
+  if (hasPermission) {
+    const device = Camera.getAvailableCameraDevices()[1];
+    return (
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+      />
+    );
+  } else {
+    return <Text>No access to camera</Text>;
+  }
+}
 
 export default CameraScreen;
