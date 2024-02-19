e813733ad4748c3c791c6e04878c2adc60b9f0cc
Ethan Soohwan Kim <106555461+EthanSKim@users.noreply.github.com>
2024-02-19 00:05:06 -0600

    Ftr20 basic homepage (#23)
    * implemented homescreen
    
    * Added icons for android
    
    ---------
    
    Co-authored-by: Braeden Smith <braedensmith629@gmail.com>
    Co-authored-by: Christian Mosquera <63211376+christianMosquera@users.noreply.github.com>

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, View } from "react-native"
+import { IconButton, MD3Colors, Text } from "react-native-paper";
+
+const TopNav = () => {
+    return (
+        <View style={styles.nav}>
+            <IconButton
+                icon={'account-plus'} 
+                size={30}
+                iconColor={MD3Colors.neutral90}
+            />
+            <Text style={styles.title}>
+                SNIPEME
+            </Text>
+            <IconButton 
+                icon={'bell'} 
+                iconColor={MD3Colors.neutral90}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    nav: {
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems:"center",
+        
+    },
+    title:{
+        fontSize:25,
+        fontWeight:"bold",
+        color: "white",
+    }
+  });
+
+export default TopNav;
