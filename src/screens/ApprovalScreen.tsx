cc9e77d3753a51e3c1706b5387e3bb93c76245a7
Braeden Smith <braedensmith629@gmail.com>
2024-03-03 13:34:57 -0600

    Sorting if multiple awaiting snipes

--- src/screens/ApprovalScreen.tsx
+++ src/screens/ApprovalScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native'
 import { Button } from 'react-native-paper';
 import { Snipe } from '../types/Snipe';
@@ -9,16 +9,20 @@ type Props = {
 };
 
 const ApprovalScreen = (props: Props) => {
+  // Keep track of the next snipe to approve
+  const [currentSnipe, setCurrentSnipe] = useState<Snipe>(props.unapprovedSnipes.sort((a, b) => a.timestamp.seconds - b.timestamp.seconds)[0]);
+  useEffect(() => {
+    setCurrentSnipe(props.unapprovedSnipes.sort((a, b) => a.timestamp.seconds - b.timestamp.seconds)[0]);
+  }, [props.unapprovedSnipes]);
 
-  return (<View style={StyleSheet.absoluteFill}>
+  return (
+  <View style={StyleSheet.absoluteFill}>
     <Image style={StyleSheet.absoluteFill} source={{ uri: props.unapprovedSnipes[0].image }} />
-    <Text>Unapproved: {props.unapprovedSnipes.length} </Text>
-    {/* <Image style={StyleSheet.absoluteFill} src={'file://' + photoPath} alt="captured photo" />
-    <Button onPress={() => setPhotoPath(null)} style={{position: 'absolute', bottom: 20, left: 50, alignSelf: 'flex-start', backgroundColor: 'white'}}>
-      <Text style={{color: 'black'}}>Retake</Text>
-    </Button> */}
+    <Button onPress={() => {}} style={{position: 'absolute', bottom: 20, left: 50, alignSelf: 'flex-start', backgroundColor: 'white'}}>
+      <Text style={{color: 'black'}}>Reject</Text>
+    </Button>
     <Button onPress={() => {props.setUnapprovedSnipes([])}} style={{position: 'absolute', bottom: 20, right: 50, alignSelf: 'flex-end', backgroundColor: 'black'}}>
-      <Text style={{color: 'white'}}>Post!</Text>
+      <Text style={{color: 'white'}}>Approve!</Text>
     </Button>
   </View>)
 }
