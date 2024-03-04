195925a021a82a739732b136e61b1b8cc1da9045
Braeden Smith <braedensmith629@gmail.com>
2024-03-03 23:00:29 -0600

    Post Approval (#52)
    * Modify index stack to check for unapproved snipes
    
    * Updated index snipe fetching logic
    
    * Added basic placeholder
    
    * Fix Snipe type timestamps
    
    * Sorting if multiple awaiting snipes
    
    * Snipe type should not include usernames
    
    * Update styles and layout
    
    * Can now approve snipes
    
    * Add reject snipe

--- src/navigation/IndexStack.tsx
+++ src/navigation/IndexStack.tsx
@@ -2,8 +2,11 @@ import React, {useEffect, useState} from 'react';
 
 import AppStack from './AppStack';
 import AuthStack from './AuthStack';
-import {FIREBASE_AUTH} from '../../firebase';
+import ApprovalScreen from '../screens/ApprovalScreen';
+import {FIREBASE_AUTH, FIREBASE_STORE} from '../../firebase';
 import {User, onAuthStateChanged} from 'firebase/auth';
+import { and, collection, getDocs, query, where } from 'firebase/firestore';
+import { Snipe } from '../types/Snipe';
 
 const IndexStack = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -16,7 +19,38 @@ const IndexStack = () => {
     // Unsubscribe when component unmounts
     return unsubscribe;
   }, []);
-  return user ? <AppStack /> : <AuthStack />;
+
+  // Check if user has unapproved snipes
+  const [unapprovedSnipes, setUnapprovedSnipes] = useState<Array<Snipe>>([]);
+  useEffect(() => {
+    if (!user) return;
+
+    // Query the database for unapproved snipes
+    const postsRef = collection(FIREBASE_STORE, "Posts");
+    const unapprovedSnipesQuery = query(postsRef, 
+      and(
+        where("target_id", "==", user.uid),
+        where("approved", "==", false)
+      ));
+
+    // Set state to false if there are no snipes, true otherwise
+    getDocs(unapprovedSnipesQuery).then((result) => {
+      const resultArray = result.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() } as Snipe;
+      });
+      setUnapprovedSnipes(resultArray);
+    });
+  }, [user]);
+
+  if (user) {
+    if (unapprovedSnipes.length > 0) {
+      return <ApprovalScreen unapprovedSnipes={unapprovedSnipes} setUnapprovedSnipes={setUnapprovedSnipes} />
+    } else {
+      return <AppStack />;
+    }
+  } else {
+    return <AuthStack />;
+  }
 };
 
 export default IndexStack;
---
+++ src/screens/ApprovalScreen.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native'
+import { Button } from 'react-native-paper';
+import { Snipe } from '../types/Snipe';
+import { FIREBASE_STORE } from '../../firebase';
+import { deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+
+type Props = {
+  unapprovedSnipes: Array<Snipe>;
+  setUnapprovedSnipes: Function;
+};
+
+const ApprovalScreen = (props: Props) => {
+  // Keep track of the next snipe to approve
+  const [currentSnipe, setCurrentSnipe] = useState<Snipe>(props.unapprovedSnipes.sort((a, b) => a.timestamp.seconds - b.timestamp.seconds)[0]);
+  useEffect(() => {
+    setCurrentSnipe(props.unapprovedSnipes.sort((a, b) => a.timestamp.seconds - b.timestamp.seconds)[0]);
+  }, [props.unapprovedSnipes]);
+
+  // Keep track of the sniper's name
+  const [sniperName, setSniperName] = useState<string>('');
+  useEffect(() => {
+    // Query the database for the sniper's name
+    const userRef = doc(FIREBASE_STORE, "Users", currentSnipe.sniper_id);
+    getDoc(userRef).then((result) => {
+      if (result.exists()) {
+        setSniperName(result.data().username);
+      }
+    });
+  }, [currentSnipe.sniper_id]);
+
+  const approveSnipe = async () => {
+    // Set approved to true in the db
+    const snipeRef = doc(FIREBASE_STORE, "Posts", currentSnipe.id);
+
+    try {
+      await updateDoc(snipeRef, { approved: true });
+
+      // Remove the snipe from the unapprovedSnipes array
+      props.setUnapprovedSnipes(props.unapprovedSnipes.filter(snipe => snipe.id !== currentSnipe.id));
+    } catch {
+      console.log('Error approving snipe');
+    }
+  }
+
+  const rejectSnipe = async () => {
+    // Remove the doc from the db
+    const snipeRef = doc(FIREBASE_STORE, "Posts", currentSnipe.id);
+
+    try {
+      await deleteDoc(snipeRef);
+
+      // Remove the snipe from the unapprovedSnipes array
+      props.setUnapprovedSnipes(props.unapprovedSnipes.filter(snipe => snipe.id !== currentSnipe.id));
+    } catch {
+      console.log('Error rejecting snipe');
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Sniped by {sniperName}!</Text>
+        <Text style={{color: 'white', fontSize: 16}}>on {new Date(currentSnipe.timestamp.seconds * 1000).toLocaleString('en-US', { month: 'long', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: true })}</Text>
+      </View>
+      <ImageBackground source={{ uri: currentSnipe.image }} style={styles.image}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={rejectSnipe} style={{backgroundColor: 'white'}}>
+            <Text style={{color: 'black'}}>Reject</Text>
+          </Button>
+          <Button onPress={approveSnipe} style={{backgroundColor: 'black'}}>
+            <Text style={{color: 'white'}}>Approve!</Text>
+          </Button>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    padding: 10,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+  },
+  buttonContainer: {
+    backgroundColor: 'rgba(100, 100, 100, 0.4)',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
+
+export default ApprovalScreen;
