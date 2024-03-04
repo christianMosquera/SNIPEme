ab4df473a5d75326ef7ac68975bc13be60440716
Ethan Soohwan Kim <106555461+EthanSKim@users.noreply.github.com>
2024-03-03 22:10:56 -0600

    Post details (#51)
    * created home stack
    
    * solved nested stack warning
    
    * completed post details page
    
    * fixed border radius
    
    * fixed photo ratio

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,69 @@
+import { Timestamp } from 'firebase/firestore';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Avatar, Button, Card, Icon, IconButton, MD3Colors, Text } from 'react-native-paper';
+import { COLORS } from '../assets/Colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { white } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+
+
+
+
+const PostDetail = ({route, navigation}: {route:any, navigation:any}) => {
+    const { snipe } = route.params;
+    const convertTimestamp = (timestamp : Timestamp) => {
+        return timestamp.toDate().toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <Card>
+                <Card.Cover source={{ uri: snipe.image }} style={{height: 495, borderRadius:0}} />
+            </Card>
+            <View style={styles.details}>
+                <View style={styles.sniperOptions}>
+                    <Text style={styles.text}>{snipe.sniper_username}</Text>
+                    <Button 
+                        mode='contained'
+                        buttonColor='white'
+                        textColor='black'
+                        style={{ borderRadius: 0 }}
+                        
+                    >
+                    Add Friend</Button>
+                </View>
+                <Text style={styles.date}>{convertTimestamp(snipe.timestamp)}</Text>
+            </View>
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.BACKGROUND,
+        flex: 1,
+        paddingTop: 65,
+    },
+    text: {
+        color: "white",
+        fontSize: 18
+    },
+    details: {
+        paddingHorizontal: 15,
+        marginTop: 25,
+    },
+    sniperOptions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:"center",
+        paddingBottom: 24
+    },
+    date: {
+        color: '#c2acac'
+    }
+  });
+
+export default PostDetail;
--- src/types/StackParamList.ts
+++ src/types/StackParamList.ts
@@ -1,7 +1,11 @@
+import { ITSnipe } from "../components/Post";
+
 export type StackParamList = {
   Welcome: undefined;
   Login: undefined;
   SignUp: undefined;
   Password: {name: string; username: string; email: string};
   Reset: undefined;
+  Main: undefined;
+  Detail: ITSnipe;
 };
