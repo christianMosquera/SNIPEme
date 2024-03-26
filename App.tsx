eab6b5a92ba3e47878f8ad7e8796c4c62f1b00ab
Braeden Smith <braedensmith629@gmail.com>
2024-03-26 00:40:50 -0500

    Ensure firebase app is always initialized at root

--- App.tsx
+++ App.tsx
@@ -9,8 +9,10 @@ import React from 'react';
 import IndexStack from './src/navigation/IndexStack';
 import {PaperProvider} from 'react-native-paper';
 import {GlobalProvider} from './src/contexts/GlobalContext';
+import { FIREBASE_APP } from './firebase';
 
 function App(): React.JSX.Element {
+  let _ = FIREBASE_APP; // Ensure Firebase is initialized
   return (
     <PaperProvider>
       <GlobalProvider>
