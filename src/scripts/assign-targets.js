1fc350f05c26a0006ffbba5d0069e911e82785b0
Braeden Smith <braedensmith629@gmail.com>
2024-04-03 12:37:21 -0500

    Take sniping status into account

--- src/scripts/assign-targets.js
+++ src/scripts/assign-targets.js
@@ -28,13 +28,17 @@ const main = async () => {
   await getDocs(usersQuery).then((snapshot) => {
     snapshot.docs.forEach((doc) => {
       const friendEntry = friendMap.find((user) => user.id === doc.id);
-      if (friendEntry) friendMap.find((user) => user.id === doc.id).zipcode = doc.data().zipcode;
+      if (friendEntry) {
+        friendEntry.zipcode = doc.data().zipcode;
+        friendEntry.allowedToSnipe = doc.data().isSnipingEnabled;
+      }
     });
   });
 
-  // Filter each user's friends to match the user's zipcode
+  // Filter each user's friends to match the user's zipcode and opted in
   friendMap.forEach((user) => {
-    user.friends = user.friends.filter((friend) => friendEntry = friendMap.find((user) => user.id === friend).zipcode === user.zipcode);
+    user.friends = user.friends.filter((friend) => friendMap.find((user) => user.id === friend).zipcode === user.zipcode);
+    user.friends = user.friends.filter((friend) => friendMap.find((user) => user.id === friend).allowedToSnipe);
   });
 
   // Assign targets to each user
