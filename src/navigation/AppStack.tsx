5e799e23eff580ef17f997f211c509fbcfafad61
Vyom Gupta <vyomsgupta@tamu.edu>
2024-04-04 18:05:13 -0500

    Create and add custom tab buttons and icons to the bottom tab navigator

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -19,8 +19,31 @@ import ProfileStackScreen from './ProfileStack';
 import { getToken, NotificationListener, requestUserPermission, setToken } from '../utils/pushnotification';
 import { UserContext } from '../contexts/UserContext';
 import {FIREBASE_AUTH} from '../../firebase';
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Tab = createBottomTabNavigator();
+const CustomTabButton = ({children, onPress}:{children:any, onPress?:any}) => (
+  <TouchableOpacity
+    style = {{
+      top: -30,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}
+    onPress={onPress}
+  >
+    <View style={{
+      width: 70,
+      height:70,
+      borderRadius: 35,
+      backgroundColor: 'transparent'
+    }}>
+      {children}
+
+    </View>
+
+  </TouchableOpacity>
+);
+
 function AppStack(): React.JSX.Element {
   const currentUser = useContext(UserContext);
   useEffect(() => {
@@ -34,10 +57,91 @@ function AppStack(): React.JSX.Element {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
-        }}>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Camera" component={CameraScreen} />
-        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: 'absolute',
+            // height: 70,
+            bottom: 10,
+            left: 20,
+            right: 20,
+            backgroundColor: "transparent",
+            borderRadius: 15,
+            opacity: 1,
+            elevation: 27,
+            borderColor: "transparent"
+
+          },
+          // tabBarActiveBackgroundColor: "lightgreen",
+          tabBarItemStyle: {
+            borderRadius: 15
+          }
+
+        }}
+        
+        >
+        <Tab.Screen name="Home" component={HomeStack} options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../assets/icons/noun-homeCrop.png')}
+                resizeMode='contain'
+                style={{
+                  width:35,
+                  height:35,
+                  tintColor: focused ? '#FF16B4' : "#FFFFFF"
+                  
+                }}
+              />
+            </View>
+          ),
+        }} />
+
+
+        <Tab.Screen name="Camera" component={CameraScreen} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+              <Image
+                source={require('../assets/icons/crosshair.png')}
+                resizeMode='contain'
+                style = {{
+                  width:85,
+                  height:85,
+                  tintColor: 'white',
+                }}
+
+              />
+              </View>
+            ),
+            tabBarButton: (props) => (
+              <CustomTabButton {...props} />
+            )
+            
+
+
+          }}
+        />
+
+
+        <Tab.Screen name="Profile" component={ProfileStackScreen} options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../assets/icons/noun-profileCrop.png')}
+                resizeMode='contain'
+                style={{
+                  width:35,
+                  height:35,
+                  tintColor: focused ? '#F1A533' : "#FFFFFF"
+                  
+                }}
+              />
+            </View>
+          ),
+        }}
+        
+        
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
