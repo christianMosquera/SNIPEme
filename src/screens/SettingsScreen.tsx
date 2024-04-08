1d895c46c7c1e37059fd43ee1a972bea74466903
Christian Mosquera <63211376+christianMosquera@users.noreply.github.com>
2024-04-08 07:06:15 -0500

    Update settings and view non-friends profile (#86)

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -7,20 +7,40 @@ import {
   TextInput,
   StyleSheet,
   SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 import {FIREBASE_AUTH} from '../../firebase';
 import {FirebaseError} from 'firebase/app';
 import {signOut} from 'firebase/auth';
 import updateZipCode from '../utils/updateZipCode';
 import {COLORS} from '../assets/Colors';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
+import {IconButton} from 'react-native-paper';
 
 const SettingsScreen = () => {
   const [newZipCode, setNewZipCode] = useState('');
 
   const handleSignOut = async () => {
     try {
-      await signOut(FIREBASE_AUTH);
-      Alert.alert('Sign Out', 'You have been successfully signed out.');
+      Alert.alert(
+        'Sign Out',
+        'Are you sure you want to sign out?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {
+            text: 'Sign out',
+            onPress: async () => {
+              await signOut(FIREBASE_AUTH);
+              Alert.alert('Sign Out', 'You have been successfully signed out.');
+            },
+          },
+        ],
+        {cancelable: false},
+      );
     } catch (error) {
       if (error instanceof FirebaseError) {
         console.error('Error signing out:', error.message);
@@ -47,19 +67,98 @@ const SettingsScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Update Zip Code</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter new zip code"
-          placeholderTextColor={COLORS.PLACEHOLDER}
-          value={newZipCode}
-          onChangeText={setNewZipCode}
-          keyboardType="number-pad"
-        />
-      </View>
-      <Button title="Update Zip Code" onPress={handleUpdateZipCode} />
-      <Button title="Sign Out" onPress={handleSignOut} />
+      <ScrollView style={styles.scroll}>
+        <TouchableOpacity style={styles.settingsButton}>
+          <View style={styles.leftContainer}>
+            <IconButton icon={'chart-bar'} size={30} iconColor={COLORS.white} />
+            <Text style={styles.text}>Activity</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <IconButton
+              icon={'chevron-right'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.settingsButton}>
+          <View style={styles.leftContainer}>
+            <IconButton
+              icon={'bell-circle-outline'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+            <Text style={styles.text}>Notifications</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <IconButton
+              icon={'chevron-right'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.settingsButton}>
+          <View style={styles.leftContainer}>
+            <IconButton
+              icon={'account-circle-outline'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+            <Text style={styles.text}>Account</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <IconButton
+              icon={'chevron-right'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.settingsButton}>
+          <View style={styles.leftContainer}>
+            <IconButton
+              icon={'help-circle-outline'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+            <Text style={styles.text}>Help</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <IconButton
+              icon={'chevron-right'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.settingsButton} onPress={handleSignOut}>
+          <View style={styles.leftContainer}>
+            <Text style={styles.text}>Log out</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <IconButton
+              icon={'chevron-right'}
+              size={30}
+              iconColor={COLORS.white}
+            />
+          </View>
+        </TouchableOpacity>
+
+        {/* <Text style={styles.text}>Update Zip Code</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter new zip code"
+            placeholderTextColor={COLORS.PLACEHOLDER}
+            value={newZipCode}
+            onChangeText={setNewZipCode}
+            keyboardType="number-pad"
+          />
+        </View>
+        <Button title="Update Zip Code" onPress={handleUpdateZipCode} />
+        <Button title="Sign Out" onPress={handleSignOut} /> */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -88,7 +187,32 @@ const styles = StyleSheet.create({
   text: {
     color: COLORS.white,
     fontSize: 20,
-    marginBottom: 20,
+    marginBottom: 0,
+    marginLeft: 10,
+  },
+  scroll: {
+    width: '100%',
+    marginTop: 50,
+  },
+  settingsButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    width: '95%',
+    alignSelf: 'center',
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Updated alignment
+  },
+  rightContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Updated alignment
+  },
+
+  help: {
+    color: COLORS.white,
   },
 });
 
